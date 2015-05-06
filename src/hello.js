04f03f5203570fa4dfee39a9d03db33552402c4c
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2015-05-06 20:23:55 -0300

    testando JS

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,20 @@
-function greet(name) {
-	return 'Hello, ' + name + '!';
+var Ponto = function(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.toString = function() {
+		return '(' + this.x + ',' + this.y + ')';
+	}
+
+	return this;
 }
 
-module.exports.greet = greet;
+function mundo_pequeno(ponto_atual,outros_pontos) {
+	var proximos = [];
+	
+
+	return ponto_atual.toString();
+}
+
+module.exports.mundo_pequeno = mundo_pequeno;
+module.exports.Ponto = Ponto;
--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,8 +1,19 @@
 var assert = require('assert');
 var hello = require('../src/hello');
 
-describe('greeter', function(){
-	it('should say Hello to the World', function() {
-		assert.equal(hello.greet('World'),'Hello, World!');
+describe('mundo_pequeno', function(){
+	it('deveria retornar o ponto atual', function() {
+		var p = new hello.Ponto(1,1);
+		assert.equal(p.toString(),'(1,1)');
 	});
+	it('deveria retornar os proximos pontos', function() {
+		var proximos = [
+			new hello.Ponto(1,1),
+			new hello.Ponto(1,2),
+			new hello.Ponto(2,1)
+		];
+
+		assert.equal(proximos.toString(),'(1,1),(1,2),(2,1)');
+	});
+
 });
