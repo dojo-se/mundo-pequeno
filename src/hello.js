ea8c8b43e97da4621956cbef45e69f8859c67cb3
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2015-05-06 21:17:21 -0300

    Funcionando

--- src/hello.js
+++ src/hello.js
@@ -10,7 +10,23 @@ var Ponto = function(x, y) {
 }
 
 function mundo_pequeno(ponto_atual,outros_pontos) {
-	return outros_pontos.slice(0,3);
+	var pontos_com_distancia = outros_pontos.map(function(ponto){
+		var dx = Math.pow(ponto_atual.x - ponto.x,2);
+		var dy = Math.pow(ponto_atual.y - ponto.y,2);
+		return {'distancia' : Math.sqrt(dx + dy),
+		 				'ponto' : ponto};
+	});
+
+	pontos_ordenados = pontos_com_distancia.sort(function(p1,p2){
+		return p1.distancia - p2.distancia;
+	})
+
+	var amigos_proximos = pontos_ordenados.slice(0,3);
+
+	// Pequena gambi
+	return amigos_proximos.map(function(ponto_com_distancia){
+		return ponto_com_distancia.ponto;
+	});
 }
 
 module.exports.mundo_pequeno = mundo_pequeno;
--- test/hello.spec.js
+++ test/hello.spec.js
@@ -11,20 +11,20 @@ describe('mundo_pequeno', function(){
 		var proximos = [
 			new hello.Ponto(1,1),
 			new hello.Ponto(1,2),
-			new hello.Ponto(2,1)
+			new hello.Ponto(1,3)
 		];
-		assert.equal(proximos.toString(),'(1,1),(1,2),(2,1)');
+		assert.equal(proximos.toString(),'(1,1),(1,2),(1,3)');
 	});
 
 	it('amigos obvios', function() {
 		var amigos = [
 			new hello.Ponto(1,2),
 			new hello.Ponto(1,3),
-			new hello.Ponto(2,1)
+			new hello.Ponto(1,4)
 		];
 		var eu = new hello.Ponto(1,1);
 		var proximos = hello.mundo_pequeno(eu, amigos);
-		assert.equal(proximos.toString(), '(1,2),(1,3),(2,1)');
+		assert.equal(proximos.toString(), '(1,2),(1,3),(1,4)');
 	});
 
 	it('quantidade maxima 3 amigos proximos', function(){
@@ -47,6 +47,20 @@ describe('mundo_pequeno', function(){
 			new hello.Ponto(2,4),
 			new hello.Ponto(9,5),
 			new hello.Ponto(19,50),
+			new hello.Ponto(90,55)
+		];
+		var eu = new hello.Ponto(1,1);
+		var proximos = hello.mundo_pequeno(eu, outros);
+		assert.equal(proximos.toString(), '(2,2),(2,3),(2,4)');
+	});
+
+	it('amigos mais proximos da zueira', function() {
+		var outros = [
+			new hello.Ponto(2,2),
+			new hello.Ponto(19,50),
+			new hello.Ponto(2,3),
+			new hello.Ponto(2,4),
+			new hello.Ponto(9,5),
 			new hello.Ponto(90,55)
 		];
 		var eu = new hello.Ponto(1,1);
